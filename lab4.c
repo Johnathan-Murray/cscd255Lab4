0321a4c539ea7654fbebe9e9efd5551b055d98df
Johnny Murray <jmurray17@ewu.edu>
2020-10-09 20:44:38 +0000

    Stubbed out the functions

--- lab4.c
+++ lab4.c
@@ -1,2 +1,26 @@
 #include "lab4.h"
 
+int readMeterValue(char * str)
+{
+return 0;
+}
+
+double determineUsage(int begMeterValue, int endMeterValue)
+{
+  return 0.0;
+}
+
+char readCustomerCode()
+{
+  return '0';
+}
+
+double determineCost(double usage, char customerCode)
+{
+  return 0.0;
+}
+
+void printResults(double cost, double usage, char customerCode, int begMeterValue, int endMeterValue)
+{
+
+}
--- lab4.h
+++ lab4.h
@@ -4,7 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+int readMeterValue(char * str);
+double determineUsage(int begMeterValue, int endMeterValue);
+char readCustomerCode();
+double determineCost(double usage, char customerCode);
+void printResults(double cost, double usage, char customerCode, int begMeterValue, int endMeterValue);
 
 
 #endif
